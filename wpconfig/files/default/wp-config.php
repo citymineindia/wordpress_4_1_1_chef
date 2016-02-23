091ec57ee45dd90a9fcda286908f6b2cc676ab0d
citymineindia <citymineindia@gmail.com>
2016-02-23 19:45:11 +0530

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'citymine-001.yvfyxo.0001.use1.cache.amazonaws.com');
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','CityMine-1670928241.us-east-1.elb.amazonaws.com');
+define('WP_SITEURL','CityMine-1670928241.us-east-1.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
